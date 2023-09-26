63274af154e5235cfef1b528341577807c6e3deb
muli338 <67047426+muli338@users.noreply.github.com>
2023-09-26 13:22:32 +0300

    first commit

---
+++ includes/navigation.php
@@ -0,0 +1,16 @@
+                 <div class="navbar-brand ">
+                    <a href="../index.php">
+                          <!-- Dark Logo icon -->
+                                <img src="../assets/images/logo-icon-nav.png" alt="homepage" class="dark-logo" />
+                                <!-- Light Logo icon -->
+                            </b>
+                            <!--End Logo icon -->
+                            <!-- Logo text --><!-- Logo icon -->
+                            <b class="logo-icon">
+                              
+                            <span class="logo-text text-dark text-truncate">
+                               Atlanta Hostels
+                            </span>
+                        </a>
+                    </div>
+<h1 class="text-dark mb-1 text-truncate font-weight-medium text-center">Welcome to Atlanta hostels Registration</h1>
